343ce46feb55f1da7282550b67b6c37f31cc3847
Alessandro Moraes <acampanhademoraes@infomedia.com.au>
2021-02-26 09:31:12 +1100

    Initial commit

---
+++ src/main/java/com/github/amoraes/slowwebserver/SlowWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.amoraes.slowwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowWebServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/amoraes/slowwebserver/SlowWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.amoraes.slowwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
