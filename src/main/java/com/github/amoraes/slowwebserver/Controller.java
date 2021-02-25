1ce895f9057d3586119a3aba099da478141c23de
Alessandro Moraes <acampanhademoraes@infomedia.com.au>
2021-02-26 09:49:49 +1100

    Add endpoint that always respond with 500 after something between 1 and 10 seconds

---
+++ src/main/java/com/github/amoraes/slowwebserver/Controller.java
@@ -0,0 +1,32 @@
+package com.github.amoraes.slowwebserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+public class Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+
+    private IntRandomNumberGenerator intRandomNumberGenerator =
+            new IntRandomNumberGenerator(1, 10);
+
+    @RequestMapping("/**")
+    public Mono<ResponseEntity<String>> hello(ServerHttpRequest request) {
+        int delay = intRandomNumberGenerator.nextInt();
+        log.info("{} {} received (reponse will take {} seconds)", request.getMethodValue(), request.getPath(), delay);
+        return Mono.delay(Duration.ofSeconds(delay))
+                .thenReturn("error!")
+                .map(value -> {
+                    return ResponseEntity.status(500).build();
+                });
+    }
+
+}
